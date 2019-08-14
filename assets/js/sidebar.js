e839807e6050c0216e814531c40b14ccfeeb78bc
FrancescoMagliocco <Magliocco.W.Francesco@gmail.com>
2019-08-14 00:26:18 -0400

    Refactored

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,14 +1,15 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
+var l = dropdown.length;
 
-for (i = 0; i < dropdown.length; i++) {
+for (i = 0; i < l; i++) {
   dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
   });
 }
